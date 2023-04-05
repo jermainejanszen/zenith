e6769772b40ef02c873aaca9828d07849fe66a94
Jermaine Janszen <jermainejanszen@gmail.com>
2023-04-05 19:00:34 +1000

    Initial sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import SidebarWithHeader from "~/components/SideBar";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,11 @@ const Home: NextPage = () => {
         <meta name="description" content="Frenchs Forest Sydney Australia" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center"></main>
+      <main className="min-h-screen w-screen">
+        <SidebarWithHeader>
+          <></>
+        </SidebarWithHeader>
+      </main>
     </>
   );
 };
